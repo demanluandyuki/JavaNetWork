f1089f4e4fe605edbe1f9e4caae20f90d28133b2
luyuanchun032 <luyuanchun032@pingan.com.cn>
2016-01-08 08:59:25 +0800

    add

---
+++ jvmtestlib/src/main/java/com/joyfulmath/ITestInterface.java
@@ -0,0 +1,9 @@
+package com.joyfulmath;
+
+/**
+ * @author deman.lu email luyuanchun032@pingan.com.cn
+ * @version on 2016-01-05 16:48
+ */
+public interface ITestInterface {
+    void test(float f);
+}
---
+++ jvmtestlib/src/main/java/com/joyfulmath/TestClass.java
@@ -0,0 +1,14 @@
+package com.joyfulmath;
+
+public class TestClass implements ITestInterface{
+    private int m;
+    public int inc()
+    {
+        return m+1;
+    }
+
+    @Override
+    public void test(float f) {
+        
+    }
+}
